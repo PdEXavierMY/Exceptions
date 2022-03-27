6fe2f199193553332eb6a8227fd2c74adf03fb79
Xavitheforce <javier.miguelez.yague@gmail.com>
2022-03-27 13:46:16 +0200

    codigo terminado

--- excepcions.py
+++ excepcions.py
@@ -1,32 +1,33 @@
 import re
 from introducir import solicitar_introducir_cadena
 
-def cuenta_electronica():
-    correo = solicitar_introducir_cadena("Introduzca su correo")
-    comprobacion = comprobar(correo)
-    if comprobacion == False:
-        print("Direccion no válida. Por favor introduzca un correo del formato xxx@xxx.xxx")
-        cuenta_electronica()
-    else:
-        if comprobacion == 1:
-            usuario = []
-            nombre = correo.split("@")
-            usuario.append(nombre[0])
-            print("¡Bienvenido "+str(usuario[0])+"!")
-        elif comprobacion == 2:
-            print("Extensión no reconocida, solo se admite '.com' o '.es'\nCuenta bloqueada a causa de un posible ataque.")
-            exit()
-
-def comprobar(correo):
-    escorreo = re.search(".*@.*\..*", correo)
-    if escorreo == None:
-        return False
-    else:
-        escorreo1 = re.search(".com$", correo)
-        escorreo2 = re.search(".es$", correo)
-        if escorreo1 or escorreo2 != None:
-            return 1
+class correo_electronico:
+    def cuenta_electronica():
+        correo = solicitar_introducir_cadena("Introduzca su correo")
+        comprobacion = correo_electronico.comprobar(correo)
+        if comprobacion == False:
+            print("Direccion no válida. Por favor introduzca un correo del formato xxx@xxx.xxx")
+            correo_electronico.cuenta_electronica()
         else:
-            return 2
+            if comprobacion == 1:
+                usuario = []
+                nombre = correo.split("@")
+                usuario.append(nombre[0])
+                print("¡Bienvenido "+str(usuario[0])+"!")
+            elif comprobacion == 2:
+                print("Extensión no reconocida, solo se admite '.com' o '.es'\nCuenta bloqueada a causa de un posible ataque.")
+                exit()
 
-cuenta_electronica()
+    def comprobar(correo):
+        escorreo = re.search(".*@.*\..*", correo)
+        if escorreo == None:
+            return False
+        else:
+            escorreo1 = re.search(".com$", correo)
+            escorreo2 = re.search(".es$", correo)
+            if escorreo1 or escorreo2 != None:
+                return 1
+            else:
+                return 2
+
+correo_electronico.cuenta_electronica()
