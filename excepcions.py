83099147df80fc9bd7fda0d6937ebda876322069
Xavitheforce <javier.miguelez.yague@gmail.com>
2022-03-27 12:06:59 +0200

    Update excepcions.py

--- excepcions.py
+++ excepcions.py
@@ -3,6 +3,15 @@ from introducir import solicitar_introducir_cadena
 
 def cuenta_electronica():
     correo = solicitar_introducir_cadena("Introduzca su correo")
-    print(comprobar(correo))
+    if comprobar(correo) == False:
+        print("Direccion no válida. Por favor introduzca un correo del formato xxx@xxx.xxx")
+        cuenta_electronica()
+    else:
+        print("correcto!")
 
 def comprobar(correo):
+    escorreo = re.search(".*@.*\..*", correo)
+    if escorreo == None:
+        return False
+    else:
+        return True
